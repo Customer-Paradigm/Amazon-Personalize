2e7a61d1ef8e567d59a84c6de2a1d3d2ad91f15d
demo <jeff@customerparadigm.com>
2022-07-08 20:39:54 +0000

    changed table creation method to declarative

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -31,44 +31,6 @@ class InstallSchema implements InstallSchemaInterface
 
     public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
-        $installer = $setup;
-        $installer->startSetup();
-
-        $table = $installer->getConnection()->newTable($installer->getTable('aws_predicted_items'))->addColumn(
-            'recommendation_id',
-            Table::TYPE_INTEGER,
-            null,
-            ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
-            'Recommendation Id'
-        )->addColumn(
-            'user_id',
-            Table::TYPE_INTEGER,
-            null,
-            ['nullable' => false],
-            'User Id'
-        )->addColumn(
-            'item_type',
-            Table::TYPE_TEXT,
-            255,
-            ['nullable' => false],
-            'Item Type'
-        )->addColumn(
-            'item_list',
-            Table::TYPE_TEXT,
-            '2M',
-            ['nullable' => false],
-            'Item List'
-        )->addColumn(
-            'updated_at',
-            Table::TYPE_TIMESTAMP,
-            null,
-            ['nullable' => false, 'default' => Table::TIMESTAMP_INIT_UPDATE],
-            'Updated At'
-        );
-        $installer->getConnection()->createTable($table);
-	$installer->endSetup();
-	//$this->configWriter->save('awsp_settings/awsp_general/ec2_install', 0, $scope = ScopeConfigInterface::SCOPE_TYPE_DEFAULT, 0);
 	$this->awsHelper->populateEc2CheckVal();
-
     }
 }
